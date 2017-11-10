d6a1723ff91b4d217cb5f4332753c535fe80ab95
jrbeverly <jrbeverly@live.com>
2017-11-09 22:07:26 -0500

    feat: add book component to handle display of book

---
+++ src/components/Book.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class Book extends Component {
+
+    static propTypes = {
+        title: PropTypes.string,
+        url: PropTypes.string,
+        authors: PropTypes.string,
+    }
+
+    render() {
+        const { title, url, authors } = this.props
+
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{ backgroundImage: `url(${url})` }}></div>
+                        <div className="book-shelf-changer">
+                            <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{title}</div>
+                    <div className="book-authors">{ authors }</div>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Book
