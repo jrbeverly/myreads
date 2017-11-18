99ed2a91afb02894696f95647e36091f29bf1259
jrbeverly <jrbeverly@live.com>
2017-11-18 15:38:55 -0500

    feat: Add searching of books by name

--- src/views/BookSearch.js
+++ src/views/BookSearch.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+import Book from 'components/Book.js'
+
+import * as BooksAPI from 'api/BooksAPI'
 
 class BookSearch extends Component {
 
+    state = {
+        query: '',
+        books: []
+    }
+
+    getBooks(query) {
+        BooksAPI
+            .search(query, 10)
+            .then((books) => {
+                this.setState({ books: books })
+            });
+    }
+
+    onQueryChange = (e) => {
+        const query = e.target.value.trim();
+        this.setState({
+            query: query
+        });
+
+        this.getBooks(query);
+    }
+
     render() {
+        const { query, books } = this.state
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className="close-search" to="/">Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text"
+                            placeholder="Search by title or author"
+                            value={query}
+                            onChange={this.onQueryChange} />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <div>
+                        <div className=''>
+                            <h3>Search returned {books.length} books </h3>
+                        </div>
+                        <ol className="books-grid">
+                            {
+                                (books || []).map((book, index) => (
+                                    <Book
+                                        key={index}
+                                        book={book}
+                                    />
+                                ))}
+                        </ol>
+                    </div>
                 </div>
             </div>
         )
