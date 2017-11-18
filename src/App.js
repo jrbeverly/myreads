9cd972658eb944ae7bad345cde19fedad93d58b3
jrbeverly <jrbeverly@live.com>
2017-11-18 13:24:44 -0500

    chore: Move high level views to view directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import BookSearch from './components/BookSearch.js'
-import BookList from './components/BookList.js'
+import BookSearch from 'views/BookSearch.js'
+import BookList from 'views/BookList.js'
 
 import './App.css'
 
